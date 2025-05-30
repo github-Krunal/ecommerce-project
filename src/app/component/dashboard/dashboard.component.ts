a6b30d31089befe42858d81b8a75632e1059dfe8
githubkrunal <githubkrunal@gmail.com>
2025-05-30 09:09:20 +0530

    service created

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { BannerofferComponent } from "./banneroffer/banneroffer.component";
 
+const COMPONENTS_LIST=[
+  BannerofferComponent
+]
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
+    imports: [COMPONENTS_LIST]
 })
 export class DashboardComponent {
 
