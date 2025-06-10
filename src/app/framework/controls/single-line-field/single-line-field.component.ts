01d2bb6457f67892b311f2765901d8a1a5e60c24
githubkrunal <githubkrunal@gmail.com>
2025-06-10 22:27:09 +0530

    formcontrol added

--- src/app/framework/controls/single-line-field/single-line-field.component.ts
+++ src/app/framework/controls/single-line-field/single-line-field.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FieldDefination } from '../../../model/fieldDefination.interface';
 
 @Component({
   selector: 'single-line-field',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './single-line-field.component.html',
-  styleUrl: './single-line-field.component.scss'
+  styleUrl: './single-line-field.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA]
+
 })
 export class SingleLineFieldComponent {
 
+  @Input() field!:FieldDefination;
+  @Input() frameworkForm:FormGroup|undefined;
+
+  ngOnInit(): void {
+    this.addControlInForm();
+  }
+   private addControlInForm():void{
+    this.frameworkForm?.addControl(this.field.formControlName, new FormControl(''));
+   }
 }
--- src/app/framework/framework.component.ts
+++ src/app/framework/framework.component.ts
@@ -1,8 +1,10 @@
+import { FieldDefination } from './../model/fieldDefination.interface';
 import { CommonModule } from '@angular/common';
 import { Component, Input,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IBusinessObject } from '../model/businessObject.interface';
 import { ControlsModule } from './controls/controls.module';
+import { FieldTypeEnum } from '../enum/fieldType.enum';
 @Component({
   selector: 'app-framework',
   standalone: true,
@@ -12,10 +14,31 @@ import { ControlsModule } from './controls/controls.module';
   schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA]
 })
 export class FrameworkComponent {
+
   @Input() businessObject:IBusinessObject | undefined;
   public frameworkForm: FormGroup | undefined;
+  protected fieldDefination:FieldDefination[]=[];
+  protected fieldTypeEnum = FieldTypeEnum;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.generateFrameworkForm();
+    this.getFieldDefination();
+  }
+
+  private getFieldDefination(): void {
+    if (this.businessObject?.fieldDefination) {
+      this.fieldDefination = this.businessObject?.fieldDefination
+    }
+  }
+
+  private generateFrameworkForm(): void {
+    this.frameworkForm = this.formBuilder.group({});
+  }
 
   public onSubmitFrameworkForm(){
-
+    debugger
+    console.log(this.frameworkForm);
   }
 }
