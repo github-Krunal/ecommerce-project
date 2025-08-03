01c757459faf1c9ad52a0b7271e4c124c9ece4ed
githubkrunal <githubkrunal@gmail.com>
2025-08-03 16:12:29 +0530

    no record found component added

---
+++ src/app/global-component/no-record-found/no-record-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-record-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-record-found.component.html',
+  styleUrl: './no-record-found.component.scss'
+})
+export class NoRecordFoundComponent {
+
+}
