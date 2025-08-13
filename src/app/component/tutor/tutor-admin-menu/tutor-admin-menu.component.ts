00f6b916bd474b392669f28e6f812cc27fc2a686
githubkrunal <githubkrunal@gmail.com>
2025-08-13 09:04:22 +0530

    menu added

--- src/app/component/tutor/tutor-admin-menu/tutor-admin-menu.component.ts
+++ src/app/component/tutor/tutor-admin-menu/tutor-admin-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tutor-admin-menu',
+  selector: 'tutor-admin-menu',
   standalone: true,
   imports: [],
   templateUrl: './tutor-admin-menu.component.html',
--- src/app/component/tutor/tutor-admin/tutor-admin.component.ts
+++ src/app/component/tutor/tutor-admin/tutor-admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TutorAdminMenuComponent } from '../tutor-admin-menu/tutor-admin-menu.component';
 
 @Component({
   selector: 'app-tutor-admin',
   standalone: true,
-  imports: [],
+  imports: [TutorAdminMenuComponent],
   templateUrl: './tutor-admin.component.html',
   styleUrl: './tutor-admin.component.scss'
 })
