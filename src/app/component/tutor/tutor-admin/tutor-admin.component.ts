e9f5b48cc143f5fd8bcf6d07f72940a2a0bc85be
githubkrunal <githubkrunal@gmail.com>
2025-08-13 09:08:26 +0530

    courses component creation

--- src/app/component/tutor/tutor-admin/tutor-admin.component.ts
+++ src/app/component/tutor/tutor-admin/tutor-admin.component.ts
@@ -1,10 +1,11 @@
+import { TutorCoursesComponent } from './../tutor-courses/tutor-courses.component';
 import { Component } from '@angular/core';
 import { TutorAdminMenuComponent } from '../tutor-admin-menu/tutor-admin-menu.component';
 
 @Component({
   selector: 'app-tutor-admin',
   standalone: true,
-  imports: [TutorAdminMenuComponent],
+  imports: [TutorAdminMenuComponent,TutorCoursesComponent],
   templateUrl: './tutor-admin.component.html',
   styleUrl: './tutor-admin.component.scss'
 })
---
+++ src/app/component/tutor/tutor-courses/tutor-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tutor-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutor-courses.component.html',
+  styleUrl: './tutor-courses.component.scss'
+})
+export class TutorCoursesComponent {
+
+}
